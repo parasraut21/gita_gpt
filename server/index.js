1f33da06b926f871e50c5ee4e9fa0acc578b4d8b
Paras Raut <parasraut2511@gmail.com>
2023-11-27 19:36:02 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -13,13 +13,14 @@ let dataBuffer = fs.readFileSync('./gita_pdf1.pdf');
 pdfParse(dataBuffer).then(function(data) {
     gitaText = data.text.toLowerCase();
 });
-
 app.post('/api/query', (req, res) => {
   const queryWords = req.body['query'].toLowerCase().split(' ');
   const lines = gitaText.split('\n');
 
+  console.log('Query:', queryWords);
+
   let maxMatchCount = 0;
-  let bestMatch = 'Question not found in Gita';
+  let bestMatch = "In the realm of my knowledge, it seems this particular query is not tucked away in the data I've been trained on. But fear not, I'm still here to chat or assist in any way I can on other topics!";
 
   for (let i = 0; i < lines.length; i++) {
     if (lines[i].startsWith('q: ')) {
@@ -32,11 +33,11 @@ app.post('/api/query', (req, res) => {
       }
       if (matchCount > maxMatchCount) {
         maxMatchCount = matchCount;
-        let answer = lines[i];
+        let answer = '';
         for (let j = i + 1; j < lines.length && !lines[j].startsWith('q: '); j++) {
           answer += '\n' + lines[j];
         }
-        bestMatch = answer;
+        bestMatch = answer.replace('a: ', '').trim(); // Remove 'a: ' prefix and leading and trailing whitespace
       }
     }
   }
@@ -44,6 +45,6 @@ app.post('/api/query', (req, res) => {
   res.json({ response: bestMatch });
 });
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log('Server is running on port 3000');
 });
